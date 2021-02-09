95a3752393352732c199b914dc74572742ebd5c2
yobilyo <bbb.yousfi@gmail.com>
2021-02-09 18:13:33 +0100

    fix conflict navbar

--- vue/vue_navbar.php
+++ vue/vue_navbar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <link rel="stylesheet" href="vue/navbar.css"/>
 
 <br/><br/><br/>
@@ -13,30 +12,11 @@
 			<li><a  class="lien_dropdown" href="index.php?page=31">Listes des projets</a></li>
 		</ul>
 	</li> | 
-	<li><a href="index.php?page=4">Don</a></li> | 
+	<li><a href="index.php?page=4">Dons</a></li> | 
+	<li><a href="index.php?page=5">Commentaires</a></li> | 
+	<li><a href="index.php?page=6">Associations</a></li> | 
 	<li><a href="index.php?page=90">Mon Compte</a></li> | 
 
-=======
-<nav>
-	<div>
-		<a href="index.php?page=0"><img src=" " alt="Accueil"/></a>
-	</div> | 
-	<div>
-		<a href="index.php?page=2">Utilisateurs</a>
-	</div> | 
-	<div>
-		<a href="index.php?page=3">Projets</a>
-	</div> | 
-	<div>
-		<a href="index.php?page=6">Association</a>
-	</div> | 
-	<div>
-		<a href="index.php?page=90">Mon Compte</a>
-	</div>
-	<div>
-		<a href="index.php?page=5">Commentaire</a>
-	</div>
->>>>>>> 97ba5838d16c5cf7669f6a2444c76901f5849889
 	<?php
 		if (isset($_SESSION['email']))
 		{
