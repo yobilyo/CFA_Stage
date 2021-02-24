a45104bc7c43ed10e4b0b768a89fa504772389e9
yobilyo <bbb.yousfi@gmail.com>
2021-02-24 14:07:35 +0100

    formulaire paiement fonctionnel

--- gestion_don_faire_un_don.php
+++ gestion_don_faire_un_don.php
@@ -27,8 +27,14 @@
         require_once("vue/vue_insert_don_faire_un_don.php"); 
 
         // pour l'instant seulement disponible pour les utilisateurs déjà authentifiés/connectés (pas de don anonyme):
-        if (isset($_POST['validerfairedon'])){
+        if (isset($_POST['ok2'])){
             //var_dump($_POST);
+
+            // valeurs par défaut codées en dur
+            $date = date("Y-m-d");
+            $statut = "en_attente";
+            $id_Association = 1;
+
             $unControleur->setTable ("don");
             $tab=array(
                 "montant"=>$_POST['montant'],
@@ -38,12 +44,12 @@
                 "id_Utilisateur"=>$_SESSION['id'], // session pas post
                 "id_Projet"=>$_POST['id_Projet'],
                 "id_Mode_de_paiement"=>$_POST['id_Mode_de_paiement'],
-                "id_Association"=>$_POST['id_Association']  
+                "id_Association"=>$id_Association  
             );
             $unControleur->insert($tab);
 
             // après que le don soit fait avec succès, récupéaration de l'id du don
-            // et aller vers la page de génération d'un reçu
+            // et proposer d'aller vers la page de génération d'un reçu
             $unControleur->setTable ("don");
             $where = array(
                 "montant"=>$_POST['montant'],
@@ -53,16 +59,22 @@
                 "id_Utilisateur"=>$_SESSION['id'], // session pas post
                 "id_Projet"=>$_POST['id_Projet'],
                 "id_Mode_de_paiement"=>$_POST['id_Mode_de_paiement'],
-                "id_Association"=>$_POST['id_Association']  
+                "id_Association"=>$id_Association  
             );
             $monDon = $unControleur->selectWhere($where);
 
-            $idDon = $monDon['id']; // id du don inséré obtenu par un selectWhere
+            if (isset($monDon) && $monDon['id'] != null) {
+                $idDon = $monDon['id']; // id du don inséré obtenu par un selectWhere
 
-            // transférer l'id du don dans le $GET
-            echo "<div>Don fait avec succès !</div>
-            <br/>
-            <a src='index.php?page=42&iddon=".$idDon."'>Cliquez ici pour télécharger un reçu de votre don</a>";
+                // transférer l'id du don dans le $GET
+                echo "<div>Don fait avec succès !</div>
+                <br/>
+                <a href='index.php?page=42&iddon=".$idDon."'>
+                    Cliquez ici pour télécharger un reçu de votre don
+                </a>";
+            } else {
+                echo "erreur lors de la transaction, opération annulée.";
+            }
         }
     }
 ?>
--- vue/vue_insert_don_faire_un_don.php
+++ vue/vue_insert_don_faire_un_don.php
@@ -4,6 +4,54 @@
 	<div class="row">
 
         <?php
+        echo"
+        <div class='col-sm-4'>
+            <!-- //https://getbootstrap.com/docs/4.0/components/forms/ -->
+            <form method='post' action=''>
+                <div>
+                    <h3>(1/3) Choix du Projet</h3><br/>
+                </div>
+                <!-- id du nouveau don à insérer codée en dur null -->
+                <div class='form-group'>Appréciation :
+                    <textarea class='form-control' name='appreciation' rows='6'></textarea>
+                </div>
+                <!-- statut du don codé en dur 'en cours' par défaut -->
+                <input type='hidden' name='id_Utilisateur' value='".$_SESSION['id']."' >
+                <div class='form-group'>Projet :
+                    <select class='form-control' name='id_Projet'>";
+                    foreach ($lesProjets as $unProjet) {
+                        echo "<option value='".$unProjet['id']."'>".$unProjet['nom']." (".$unProjet['pays'].")</option>";
+                    }
+                    echo "
+                    </select>
+                </div>
+                <!-- idA_Association codé en dur 1 car on a qu'une asso -->
+
+                <div>
+                    <h3>(2/3) Votre Paiement</h3><br>
+                </div>
+                <div class='form-group'>Montant :
+                    <input type='text' name='montant' class='form-control' >
+                </div> 
+                <!-- dateDon codée en dur new Date() -->
+                <div>Mode de Paiement:<br/>";
+                foreach ($lesModesdePaiements as $unModeDePaiement) {
+                    echo "
+                    <div class='form-check form-check-inline'>
+                        <img src='".$unModeDePaiement['image_url']."' width='30'></img>
+                        <input class='form-check-input' type='radio' name='id_Mode_de_paiement' value='".$unModeDePaiement['id']."'>  
+                    </div>
+                    ";
+                }
+                echo"
+                </div>
+                <input type='submit' class='btn btn-dark' name='ok2' value='OK'>
+            </form>
+        </div>";
+
+
+        //version avec plusieurs pages non fonctionelle pour le moment
+        /*
         $tab = array();
 
         echo"
@@ -20,7 +68,7 @@
                 <!-- statut du don codé en dur 'en cours' par défaut -->
                 <input type='hidden' name='id_Utilisateur' value='".$_SESSION['id']."' >
                 <div class='form-group'>Projet :
-                    <select multiple class='form-control' name='id_Projet'>";
+                    <select class='form-control' name='id_Projet'>";
                     foreach ($lesProjets as $unProjet) {
                         echo "<option value='".$unProjet['id_Projet']."'>".$unProjet['nom']." (".$unProjet['pays'].")</option>";
                     }
@@ -74,6 +122,9 @@
         print_r($_POST);
         echo "<br/><br/>";
         print_r($tab);
+
+        */
+        //version avec plusieurs pages non fonctionelle pour le moment
 
         ?>
 	</div>
